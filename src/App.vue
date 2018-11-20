7b03ccc9b844fb6022e86898dedd23f8b3fffc43
Loris Ciprin <l.ciprin@spinalcom.com>
2018-11-20 16:31:59 +0100

    improve the component  change pull-node to pull-children  improve css

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="plugin-graph-viewer">
 
-        <top-bar :buttons="topBarButton"
+        <top-bar class="graph-manager-top-bar"
+                :buttons="topBarButton"
                  :option="graph"
         />
 
@@ -10,10 +11,10 @@
                     :buttons="sideBarButton"
                     :option="selectedNode"
             />
-            <nodes-list
+            <nodes-list class="graph-viewer"
                     @node-selected="onNodeSelected($event)"
                     @hide-bim-object="onHideBimObject($event)"
-                    @pull-node="onPullNode($event)"
+                    @pull-children="onPullNode($event)"
                     :nodes="nodes"
                     :context-ids="contextIds"/>
         </div>
@@ -53,7 +54,7 @@
                 ;
             },
             onPullNode: function (event) {
-                this.$store.commit("PULL_NODE", event);
+                this.$store.commit("PULL_CHILDREN", event);
             }
         }
     }
@@ -66,24 +67,20 @@
         box-sizing: border-box
     }
 
-    .plugin-graph-viewer {
-        height: 100%;
+    .graph-viewer {
+        border-left: 1px solid rgba(128, 128, 128, 0.64);
     }
 
-    .plugin-graph-viewer:hover{
-        height: 100% ;
+    .graph-manager-top-bar{
+
+        border-bottom: 1px solid rgba(128, 128, 128, 0.64);
     }
 
-    .docking-panel:hover * {
-        background: #0d69ca;
-    }
-
-    .docking-panel * {
-        background: #ff191c;
-    }
     .graph-manager-body {
         display: flex;
         height: 100%;
         width: 100%;
+        overflow-y: auto;
+        overflow-x: hidden;
     }
 </style>
