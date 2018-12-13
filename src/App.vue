334ba944b28b4b9d2a85f8014c0a956ac66f11bb
LorisC <loris@ciprin.fr>
2018-12-13 15:40:21 +0100

    [UPDATE] add refresh button and fix some css

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="plugin-graph-viewer">
 
-        <top-bar class="graph-manager-top-bar"
-                 :buttons="topBarButton"
-                 :option="graph"
-        />
+        <div class="graph-manager-top-bar">
 
+            <top-bar :buttons="topBarButton"
+                     :option="graph"
+                     class="graph-manager-top-tools-bar"
+            />
+
+            <spinal-icon-button
+                    class="plugin-graph-viewer-refresh"
+                    icon="refresh"
+                    tool-tip="refresh graph"
+                    v-on:click="refresh()"
+            />
+
+        </div>
         <div class="graph-manager-body">
 
             <side-bar class="graph-manager-side-bar"
@@ -14,15 +24,15 @@
             />
 
             <nodes-list class="graph-viewer"
-                        @node-selected="onNodeSelected($event)"
-                        @hide-bim-object="onHideBimObject($event)"
-                        @pull-children="onPullNode($event)"
-                        @active-node="onActiveNode($event)"
-
-                        :nodes="nodes"
-                        :context-ids="contextIds"
+                        :active-node="activeNode"
                         :childrenIds="childrenIds"
-                        :active-node="activeNode"/>
+                        :context-ids="contextIds"
+                        :nodes="nodes"
+
+                        @active-node="onActiveNode($event)"
+                        @hide-bim-object="onHideBimObject($event)"
+                        @node-selected="onNodeSelected($event)"
+                        @pull-children="onPullNode($event)"/>
 
 
         </div>
@@ -34,69 +44,86 @@
   import {
     NodeList,
     SideBar,
+    SpinalIconButton,
     TopBar
   } from "spinal-env-viewer-vue-components-lib";
   import { mapState } from 'vuex'
 
   export default {
-        name: 'graph-manager',
-        components: {
-            sideBar: SideBar,
-            TopBar: TopBar,
-            NodesList: NodeList
-        },
+    name: 'graph-manager',
+    components: {
+      SpinalIconButton,
+      sideBar: SideBar,
+      TopBar: TopBar,
+      NodesList: NodeList
+    },
 
-        computed: mapState([
-            'topBarButton',
-            'sideBarButton',
-            'nodes',
-            'selectedNode',
-            'contextIds',
-            'childrenIds',
-            'graph',
-            'activeNode'
-        ]),
+    computed: mapState( [
+      'topBarButton',
+      'sideBarButton',
+      'nodes',
+      'selectedNode',
+      'contextIds',
+      'childrenIds',
+      'graph',
+      'activeNode',
+      'reset'
+    ] ),
 
-        methods: {
-            onHideBimObject: function (event) {
-                console.log("hide bim obj event", event)
-            },
+    methods: {
+      onHideBimObject: function ( event ) {
+        console.log( "hide bim obj event", event )
+      },
 
-            onNodeSelected: function (event) {
-                this.$store.dispatch("onNodeSelected", event)
-                    .then()
-                    .catch(e => console.error(e));
-            },
+      onNodeSelected: function ( event ) {
+        this.$store.dispatch( "onNodeSelected", event )
+          .then()
+          .catch( e => console.error( e ) );
+      },
 
-            onPullNode: function (event) {
-                this.$store.commit("PULL_CHILDREN", event);
-            },
+      onPullNode: function ( event ) {
+        this.$store.commit( "PULL_CHILDREN", event );
+      },
 
-            onActiveNode: function (event) {
-                this.$store.commit('SET_ACTIVE_NODE', event)
-            },
+      onActiveNode: function ( event ) {
+        this.$store.commit( 'SET_ACTIVE_NODE', event )
+      },
 
-          isInContext: function ( childrenId, contextId ) {
-            let res = false;
+      isInContext: function ( childrenId, contextId ) {
+        let res = false;
 
-            for (let i = 0; i < childrenId.length && !res; i++) {
-              const childId = childrenId[i];
-              if (this.nodes.hasOwnProperty( childId )) {
-                const node = this.nodes[childId];
-                const contextIds = node.contextIds;
-                for (let j = 0; j < contextIds.length && !res; j++) {
-                  if (contextId === contextIds[j])
-                    res = true;
-                }
-              }
+        for (let i = 0; i < childrenId.length && !res; i++) {
+          const childId = childrenId[i];
+          if (this.nodes.hasOwnProperty( childId )) {
+            const node = this.nodes[childId];
+            const contextIds = node.contextIds;
+            for (let j = 0; j < contextIds.length && !res; j++) {
+              if (contextId === contextIds[j])
+                res = true;
             }
-
-            return res;
           }
-        },
+        }
 
+        return res;
+      },
 
+      refresh: function () {
+        this.$store.commit( 'RESET' )
+      }
+    },
+    watch: {
+      'reset': {
+        handler: function ( reset ) {
+          if (reset) {
+            this.$forceUpdate();
+            this.$store.commit( 'SET_RESET', !reset );
+          }
+        }
+      }
     }
+
+
+  }
 
 </script>
 
@@ -119,6 +146,7 @@
     }
 
     .graph-manager-top-bar {
+        display: flex;
         background-color: rgba(52, 52, 52, 0.85);
         border-bottom: 1px solid rgba(128, 128, 128, 0.64);
     }
@@ -128,6 +156,15 @@
         border-bottom: 1px solid rgba(128, 128, 128, 0.64);
     }
 
+    .plugin-graph-viewer-refresh {
+        float: right;
+        font-size: 2%;
+    }
+
+    .graph-manager-top-tools-bar {
+        float: left;
+        width: 98%;
+    }
     .graph-manager-body {
         display: flex;
         height: 100%;
