e31a5559040f3cf37621da20f4f698c271477757
LorisC <loris@ciprin.fr>
2019-02-05 12:28:12 +0100

    change scroll bar style

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
+<!--
+  - Copyright 2019 SpinalCom - www.spinalcom.com
+  -
+  -  This file is part of SpinalCore.
+  -
+  -  Please read all of the following terms and conditions
+  -  of the Free Software license Agreement ("Agreement")
+  -  carefully.
+  -
+  -  This Agreement is a legally binding contract between
+  -  the Licensee (as defined below) and SpinalCom that
+  -  sets forth the terms and conditions that govern your
+  -  use of the Program. By installing and/or using the
+  -  Program, you agree to abide by all the terms and
+  -  conditions stated or referenced herein.
+  -
+  -  If you do not agree to abide by these terms and
+  -  conditions, do not demonstrate your acceptance and do
+  -  not install or use the Program.
+  -  You should have received a copy of the license along
+  -  with this file. If not, see
+  -  <http://resources.spinalcom.com/licenses.pdf>.
+  -->
+
 <template>
     <div class="plugin-graph-viewer" :style="{height: height()}">
 
@@ -74,8 +98,8 @@
     TopBar
   } from "spinal-env-viewer-vue-components-lib";
   import {
-    mapState,
-    mapGetters
+    mapGetters,
+    mapState
   } from 'vuex'
 
   export default {
@@ -193,4 +217,25 @@
         height: 100%;
         width: 100%;
     }
+
+    .graph-viewer::-webkit-scrollbar {
+        background-color: #121212;
+    }
+
+    .graph-viewer::-webkit-scrollbar {
+        width: 5px;
+        background-color: #121212;
+
+    }
+
+    .graph-viewer::-webkit-scrollbar-track {
+        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    }
+
+    .graph-viewer::-webkit-scrollbar-thumb {
+        background-color: #737374;
+        outline: 1px solid slategrey;
+    }
+
+
 </style>
